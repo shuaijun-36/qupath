29045ca3be759b79f6f1592eae828b55c6786e05
shuaijun-36 <67297572+shuaijun-36@users.noreply.github.com>
2023-09-16 18:47:57 +0800

    Update export_annotations.groovy
    change scripts

--- export_annotations.groovy
+++ export_annotations.groovy
@@ -1,10 +1,11 @@
 def imageData = getCurrentImageData()
 
 // Define output path (relative to project)
-def outputDir = buildFilePath(PROJECT_BASE_DIR, 'export')
+def outputDir = buildFilePath(PROJECT_BASE_DIR, 'export_annotations')
 mkdirs(outputDir)
 def name = GeneralTools.getNameWithoutExtension(imageData.getServer().getMetadata().getName())
-def path = buildFilePath(outputDir, name + "-labels.png")
+// Same name with the annotated image  
+def path = buildFilePath(outputDir, name + ".png")
 
 // Define how much to downsample during export (may be required for large images)
 double downsample = 1
@@ -13,9 +14,8 @@ double downsample = 1
 def labelServer = new LabeledImageServer.Builder(imageData)
   .backgroundLabel(0, ColorTools.BLACK) // Specify background label (usually 0 or 255)
   .downsample(downsample)    // Choose server resolution; this should match the resolution at which tiles are exported
-  .addLabel('Tumor', 1)      // Choose output labels (the order matters!)
-  .addLabel('Stroma', 2)
-  .addLabel('Other', 3)
+  .addLabel('nerve', 1)      // Choose output labels (the order matters!)
+  //.addLabel('Other', 2)
   .multichannelOutput(false) // If true, each label refers to the channel of a multichannel binary image (required for multiclass probability)
   .build()
 
